7881ab98877cc0e4c7a399f9c2660b58ede4e86d
MarvinCif <mcifuentesmejia@gmail.com>
2026-01-15 11:16:08 -0600

    Update: cambios en menu.sh,N2_04_Tabla_de_multiplicar.cpp

--- src/Nivel2_ControlDeFlujo_Y_Arreglos_Bucles_Arrays_Strings_Basicos/N2_04_Tabla_de_multiplicar.cpp
+++ src/Nivel2_ControlDeFlujo_Y_Arreglos_Bucles_Arrays_Strings_Basicos/N2_04_Tabla_de_multiplicar.cpp
@@ -2,3 +2,26 @@
 Enunciado: Generar la tabla de multiplicar de un número dado.
 Autor: Marvin Cifuentes
 */
+
+#include <iostream>
+#include <limits>
+
+void Tabla(int num){
+    for(int i = 0; i < num ; i++){
+        std::cout << "| " << num*i << " |" << std::endl;
+    };
+}
+
+int main(){
+    int num = 0;
+    std::cout << "Ingrese un numero :";
+    if(!std::cin >> num){
+        std::cout << "El dato ingresado no corresponde a un numero." << std::endl;
+        std::cin.clear();
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(),' \n ');
+    }
+    else{
+        std::cout << "La tabla de multiplicar para el numero " << num << " es: " << std::endl;
+        Tabla(num);
+    }
+}
