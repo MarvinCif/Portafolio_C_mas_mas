07b6c9580862264a03432795bd10b1e8e9ae091d
MarvinCif <mcifuentesmejia@gmail.com>
2026-02-08 10:12:44 -0600

    Update: cambios en N2_05_Serie_Fibonacci.cpp,N2_06_Invertir_numero.cpp,N2_05_Serie_Fibonacci,N2_06_Invertir_numero

--- src/Nivel2_ControlDeFlujo_Y_Arreglos_Bucles_Arrays_Strings_Basicos/N2_05_Serie_Fibonacci.cpp
+++ src/Nivel2_ControlDeFlujo_Y_Arreglos_Bucles_Arrays_Strings_Basicos/N2_05_Serie_Fibonacci.cpp
@@ -2,3 +2,43 @@
 Enunciado: Imprimir los primeros N términos de la serie Fibonacci.
 Autor: Marvin Cifuentes
 */
+
+
+#include <iostream>
+#include <limits>
+
+long long Fibonacci(int num){
+    if(num <=1){
+        return num;
+    }
+    else{
+        return Fibonacci(num-1) + Fibonacci(num-2);
+    }
+}
+
+int main(){
+    long long temp;
+    unsigned int num;
+    std::cout<<"SERIE DE FIBONACCI" << std::endl;
+    while(true){
+        std::cout<<"Ingrese un numero:";
+        if(!(std::cin >> temp)){
+            std::cout<<"El valor ingresado debe ser un valor numerico y mayor o igual a cero." << std::endl;
+            std::cin.clear();
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(),'\n');
+            continue;
+        }
+        if(temp < 0){
+            std::cout<<"El valor ingresado debe ser un valor numerico y mayor o igual a cero." << std::endl;
+            std::cin.clear();
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(),'\n');
+            continue;
+        }
+        else{
+            num = temp;
+            std::cout<<"La serie de Fibonacci para el numero " << num << " es: " << Fibonacci(num) << std::endl;
+            break;
+        }
+    }
+    return 0;
+}
--- src/Nivel2_ControlDeFlujo_Y_Arreglos_Bucles_Arrays_Strings_Basicos/N2_06_Invertir_numero.cpp
+++ src/Nivel2_ControlDeFlujo_Y_Arreglos_Bucles_Arrays_Strings_Basicos/N2_06_Invertir_numero.cpp
@@ -2,3 +2,39 @@
 Enunciado: Dado un entero (ej: 123), imprimirlo al revés (321).
 Autor: Marvin Cifuentes
 */
+
+#include <iostream>
+#include <limits>
+
+long long NumeroInvertido(long long num){
+    if(num == 0) return 0;
+    long long sigBit = ((num >> 31) & 0x1) ? -1 : 1;
+    long long n = num;
+    if(n < 0) n = -n;
+    long long rev = 0;
+    while(n > 0){
+        rev = rev * 10 + (n % 10);
+        n /= 10;
+    }
+    rev *= sigBit;
+    return static_cast<int>(rev);
+}
+
+
+int main(){
+    long long num = 0;
+
+    while(true){
+        std::cout << "IMPRIMIR AL REVES UN NUMERO" << std::endl;
+        std::cout << "Ingrese un numero: ";
+        if(!(std::cin>>num)){
+            std::cout<<"El valor ingresado no corresponde a un numero, ingrese un entero de manera correcta." << std::endl;
+            std::cin.clear();
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            continue;
+        }
+        std::cout << "El numero " << num << " al revés se observa como: " << NumeroInvertido(num) << std::endl;
+        break;
+    }
+    return 0;
+}
